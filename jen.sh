fb9d5f71deba27ee033b473bdff09950fd585ae2
Gabrusb <rohansb1210@gmail.com>
2025-01-27 10:39:34 +0000

    FC

---
+++ jen.sh
@@ -0,0 +1,20 @@
+create aws instance
+devloper machine:-
+apt update
+make repository in github
+install git
+git init
+git remote add origin(paste ssh keys from github)
+git pull origin master
+enter-enter
+ssh-keygen
+cat(copy rsa.pub path and paste)
+copy the key,add to deploy keys in repo settings github 
+git pull origin master
+nano index.html(hi )
+git add .
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+git commit -m "FC"
+git push origin master(done)
+it will appear on github
